00f1846419ee26872402791f5d0f41216d179787
thisdk <thisdk@gmail.com>
2022-08-19 11:06:13 +0800

    add jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,20 @@ repositories {
 }
 
 dependencies {
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
 }
 
 tasks.withType<KotlinCompile> {
