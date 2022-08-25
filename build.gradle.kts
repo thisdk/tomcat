907f361f3b224eb8a58f2767e174571182d3d9c3
thisdk <thisdk@gmail.com>
2022-08-25 21:37:24 +0800

    remove jwt

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,13 +24,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 }
 
